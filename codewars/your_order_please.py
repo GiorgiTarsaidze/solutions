a1d5261de26d6e0897c874e47bed6368164c3592
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-04-14 20:22:01 +0400

    added the greates pythonic one liner in the universe

--- codewars/your_order_please.py
+++ codewars/your_order_please.py
@@ -1,5 +1,3 @@
 def order(sentence):
   # code here
-  lst_sentance = sentence.split(" ")
-  result = sorted(lst_sentance, key=lambda word: [int(letter) for letter in word if letter.isdigit()])
-  return " ".join(result)
+  return " ".join(sorted(sentence.split(" "), key=lambda word: [int(letter) for letter in word if letter.isdigit()]))
